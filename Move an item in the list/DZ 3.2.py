15bd3d8bd50cc7fcf6ae18593a59f8c6bdad5e99
Vladislav <vlados.vadim.khomich@gmail.com>
2026-04-13 15:55:02 +0300

    Task 1 completed

---
+++ Move an item in the list/DZ 3.2.py
@@ -0,0 +1,6 @@
+first_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+second_list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+third_list = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+third_list.insert(0,30)
+print(third_list)
+#1 заданиие
