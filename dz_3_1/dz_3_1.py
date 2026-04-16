7e1d172e3421177f4771348aa7a15ffffb4d5058
Vladislav <vlados.vadim.khomich@gmail.com>
2026-04-16 22:46:08 +0300

    task 2 complete

---
+++ dz_3_1/dz_3_1.py
@@ -0,0 +1,18 @@
+first_number = float(input("Введите первое число: "))
+second_number = float(input("Введите второе число: "))
+operation = input("Введите Операцию например (+, -, *, /): ")
+
+if operation == "+":
+    result = first_number + second_number
+elif operation == "-":
+    result = first_number - second_number
+elif operation == "*":
+    result = first_number * second_number
+elif operation == "/":
+    if second_number == 0:
+        result = "на 0 делить нельзя"
+    else:
+        result = first_number / second_number
+else:
+    result = "Неверная операция"
+#2 задание
